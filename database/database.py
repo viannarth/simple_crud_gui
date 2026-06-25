143542eed3e920be9de6e6fb54d4dcb6f478bbf3
Arthur Vianna <viannarth@gmail.com>
2026-06-25 18:39:27 -0300

    update create tables

--- database/database.py
+++ database/database.py
@@ -9,38 +9,49 @@ class Database():
         self._create()
 
     def _connect(self):
-        self.conn = sqlite3.connect(DATABASE_PATH)
+        try:
+            self.conn = sqlite3.connect(DATABASE_PATH)
+        except (sqlite3.OperationalError, sqlite3.IntegrityError) as e:
+            print("Failed to open database:", e)
 
     def _create(self):
         self._connect()
-        cur = self.conn.cursor()
         
-        sql_statements:list[str] = [
-            """CREATE TABLE IF NOT EXISTS catalogo (
-                    id INTEGER PRIMARY KEY AUTOINCREMENT,
-                    nome TEXT NOT NULL,
-                    data_nascimento TEXT NOT NULL,
-                    cpf TEXT,
-                    cnpj TEXT,
-                    contato TEXT NOT NULL
-                );""",
-            """CREATE TABLE IF NOT EXISTS endereco (
-                    rua TEXT NOT NULL,
-                    numero TEXT NOT NULL, 
-                    complemento TEXT,
-                    bairro TEXT NOT NULL,
-                    cidade TEXT NOT NULL,
-                    uf TEXT NOT NULL,
-                    cep TEXT NOT NULL
-                );"""
-        ]
+        try:
+            sql_statements:list[str] = [
+                """CREATE TABLE IF NOT EXISTS prestadores (
+                        id INTEGER PRIMARY KEY,
+                        nome TEXT NOT NULL,
+                        data_nascimento TEXT NOT NULL,
+                        cpf TEXT,
+                        cnpj TEXT,
+                        contato TEXT NOT NULL
+                    );""",
+                """CREATE TABLE IF NOT EXISTS enderecos (
+                        id INTERGER PRIMARY KEY,
+                        id_prestador INT NOT NULL,
+                        rua TEXT NOT NULL,
+                        numero TEXT NOT NULL, 
+                        complemento TEXT,
+                        bairro TEXT NOT NULL,
+                        cidade TEXT NOT NULL,
+                        uf TEXT NOT NULL,
+                        cep TEXT NOT NULL,
+                        FOREIGN KEY (id_prestador) REFERENCES prestadores (id) ON DELETE CASCADE
+                    );"""
+            ]
 
-        for statement in sql_statements:
-            cur.execute(statement)
+            for statement in sql_statements:
+                self.conn.execute(statement)
 
-        self.conn.commit()
-        self.conn.close()
+            self.conn.commit()
+            
+        except (sqlite3.OperationalError, sqlite3.IntegrityError) as e:
+            print("Failed to create tables:", e)
 
+        finally:
+            self.conn.close()
+            
 
 def main():
     db = Database()
